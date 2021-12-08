bb311d68cdf24587253bf1769507acfae98131fe
Emmanuel Akala <adekyte@gmail.com>
2021-12-08 11:37:46 +0100

    added new links ... v1

--- src/utils/portfolioData/index.js
+++ src/utils/portfolioData/index.js
@@ -1,13 +1,6 @@
 export const portfolioData = [
   {
     id: '01',
-    title: 'shop.fanny.',
-    tags: ['react', 'redux', 'mongodb', 'nodejs', 'expressjs'],
-    description: `shop.fanny is as an e-commerce store that lets you shop your favorite items. It is built ontop the MERN stack, with no payment gateway currently implemented.`, 
-    code: `https://github.com/sambabib/shop-fanny`
-  },
-  {
-    id: '02',
     title: 'myInvoice.',
     tags: ['vue', 'vuex', 'firebase'],
     description: `invoiceMe is an app that provides invoices to your clients, this invoices can be paid forward, edited, and saved.`,
@@ -15,13 +8,15 @@ export const portfolioData = [
     code: `https://github.com/sambabib/invoice-app`
   },
   {
-    id: '03',
+    id: '02',
     title: 'myMuuVees.',
     tags: ['nuxtjs'],
     description: `this is an app that shows movie plots, ratings. myMuuVees is built ontop a dummy movie api.`,
+    live: `https://mymuuvees.herokuapp.com/`,
+    code: `https://github.com/sambabib/myMuuVees`
   },
   {
-    id: '04',
+    id: '03',
     title: 'board.fanny.',
     tags: ['react'],
     description: `board.fanny is a management dashboard used to track company economics/metrics.`,
@@ -29,11 +24,18 @@ export const portfolioData = [
     code: `https://github.com/sambabib/dashboard-fanny`
   },
   {
-    id: '05',
+    id: '04',
     title: 'robofriends.',
     tags: ['react'],
     description: `robofriends was my very first react app, showing all of my friends... my robofriends.`,
     live: `https://sambabib.github.io/robofriends/`,
     code: `https://github.com/sambabib/robofriends`
+  },
+  {
+    id: '05',
+    title: 'shop.fanny.',
+    tags: ['react', 'redux', 'mongodb', 'nodejs', 'expressjs'],
+    description: `shop.fanny is as an e-commerce store that lets you shop your favorite items. It is built ontop the MERN stack, with no payment gateway currently implemented.`, 
+    code: `https://github.com/sambabib/shop-fanny`
   },
 ];
